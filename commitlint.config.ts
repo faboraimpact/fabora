bccfc6622d1d0cf25de67228d22eaf681f38558d
rayantProject <92972483+rayantProject@users.noreply.github.com>
2026-03-24 02:26:23 +0100

    feat: first commit

---
+++ commitlint.config.ts
@@ -0,0 +1,17 @@
+import type { UserConfig } from '@commitlint/types';
+
+const Configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  formatter: '@commitlint/format',
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'chore', 'style', 'refactor', 'ci', 'test', 'revert', 'perf', 'vercel'],
+    ],
+    'body-max-line-length': [2, 'always', 200],
+    'header-max-length': [2, 'always', 150],
+  },
+};
+
+export default Configuration;
